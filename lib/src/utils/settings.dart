0bd4613af22ad683c60a65695be8ea1c485caf3b
bakht200 <62112170+bakht200@users.noreply.github.com>
2022-09-13 12:18:38 +0500

    agora-chat

---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,4 @@
+/// Define App ID and Token
+const appId = 'ae54f670fc1b426d902e6361698b3a28';
+const token =
+    '007eJxTYEjj3Rjzekeo/31u4zWFfA8XH26w3dHw2qO10FlNqGTT04cKDImppiZpZuYGacmGSSZGZimWBkapZsZmhmaWFknGiUYWXuoKyUsqFJMfPfvHxMgAgSA+M4OhkTEDAwBDGiAa';
